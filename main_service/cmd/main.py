fd020b1a5dfc5999051fdae1a1090779c1696d29
Mihail Pugachev <xd393727@gmail.com>
2024-05-07 12:20:50 +0300

    create ci

--- main_service/cmd/main.py
+++ main_service/cmd/main.py
@@ -1,13 +1,25 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+import fastapi.exceptions
+from fastapi import FastAPI, Depends, HTTPException
 
 
 app = FastAPI()
 
 
+def validate(text: str):
+    if text != " ":
+        return text.upper()
+    else:
+        return HTTPException(
+            status_code=400,
+            detail="Пустые данные"
+        )
+
 @app.post("/auth/register")
-def register() -> int:
-    if token:
-        return "ok"
-    return "}{yi"
+def register(text: Annotated[str, Depends(validate)]):
+    return text
+
+
 
 
